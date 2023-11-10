ce233d6eb25ae1f3983b8dc5040e5c9afafb79c0
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-11-10 19:04:47 -0300

    Fix: add ListingComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingComponent } from './components/listing/listing.component';
+import { DetailsComponent } from './components/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listing', component: ListingComponent },
+  { path: 'details', component: DetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListingComponent } from './components/listing/listing.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingComponent,
+    UserCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
